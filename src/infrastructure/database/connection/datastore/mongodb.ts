bf4fe9164b4ecf237b849c0f34b6edc21e58cf9a
emekarr <aceanyanwu02@gmail.com>
2023-06-12 20:43:05 +0100

    abandonded desctructing for importing mongoose methods in mongodb connection

--- src/infrastructure/database/connection/datastore/mongodb.ts
+++ src/infrastructure/database/connection/datastore/mongodb.ts
@@ -1,38 +1,42 @@
-import mongooose, { connect, connection, ConnectOptions } from "mongoose";
+import mongoose, { connect, connection, ConnectOptions } from "mongoose";
 import InfoLogger from "../../../logger/InfoLogger";
 import ErrorLogger from "../../../logger/ErrorLogger";
 import config from "../../../../config";
 
-
 class MongooseConnection {
-  constructor(){
-    mongooose.set('toJSON', {
+  constructor() {
+    mongoose.set("toJSON", {
       transform: (doc, ret) => {
-        ret.id = ret._id.toString()
-        delete ret._id
-        delete ret.__v
-      }
-    })
+        ret.id = ret._id.toString();
+        delete ret._id;
+        delete ret.__v;
+      },
+    });
   }
 
   connect() {
-    connect(config.getMongoURL()!, {
-      dbName: config.getDatabaseName(),
-    } as ConnectOptions)
-      .then(() => {
-        InfoLogger.write("db connection successful");
-      })
-      .catch((err) => {
-        ErrorLogger.write(`db connection failed -- ${err.message}`);
-        process.kill(process.pid, "SIGTERM");
-      });
+    mongoose
+      .connect(config.getMongoURL()!, {
+        dbName: config.getDatabaseName(),
+      } as ConnectOptions)
+      .then(() => {})
+      .catch((err) => {});
+
+    mongoose.connection.on("connected", () => {
+      InfoLogger.write("db connection successful");
+    });
+
+    mongoose.connection.on("error", () => {
+      ErrorLogger.write(`db connection failed -- `);
+      process.kill(process.pid, "SIGTERM");
+    });
   }
 
   // FOR TESTING PURPOSES ONLY
   async dropDB() {
     // used to clear the database in a TEST environment
     if (process.env.NODE_ENV === "test") {
-      await connection.dropDatabase();
+      await mongoose.connection.dropDatabase();
     }
   }
 }
